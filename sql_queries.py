440d7098920410a1deca455cf7154d44c4fcadd8
Scott Fitzgerald <sfitzgerald@eero.com>
2019-09-07 02:49:55 -0500

    load data from staging tables to analytics tables
    rename columns and use jsonpaths file for staging tables
    use correct data types on tables
    write sql for table insert statements

--- sql_queries.py
+++ sql_queries.py
@@ -15,25 +15,25 @@ drop_table_sql = 'DROP TABLE IF EXISTS events_stage, songs_stage, songplays, use
 # CREATE TABLES
 staging_events_table_create = """
 CREATE TABLE events_stage(
-    artist         VARCHAR           ENCODE ZSTD,
-    auth           VARCHAR           ENCODE ZSTD,
-    firstName      VARCHAR           ENCODE ZSTD,
-    gender         VARCHAR           ENCODE ZSTD,
-    itemInSession  INTEGER           ENCODE ZSTD,
-    lastName       VARCHAR           ENCODE ZSTD,
-    length         DOUBLE PRECISION  ENCODE ZSTD,
-    level          VARCHAR           ENCODE ZSTD,
-    location       VARCHAR           ENCODE ZSTD,
-    method         VARCHAR           ENCODE ZSTD,
-    page           VARCHAR           ENCODE ZSTD,
-    registration   DOUBLE PRECISION  ENCODE ZSTD,
-    sessionId      INTEGER           ENCODE ZSTD,
-    song           VARCHAR           ENCODE ZSTD,
-    status         VARCHAR           ENCODE ZSTD,
-    ts             BIGINT            ENCODE ZSTD,
-    userAgent      VARCHAR           ENCODE ZSTD,
-    userId         INTEGER           ENCODE ZSTD
-)
+    artist           VARCHAR           ENCODE ZSTD,
+    auth             VARCHAR           ENCODE ZSTD,
+    first_name       VARCHAR           ENCODE ZSTD,
+    gender           VARCHAR           ENCODE ZSTD,
+    item_in_session  INTEGER           ENCODE ZSTD,
+    last_name        VARCHAR           ENCODE ZSTD,
+    length           DOUBLE PRECISION  ENCODE ZSTD,
+    level            VARCHAR           ENCODE ZSTD,
+    location         VARCHAR           ENCODE ZSTD,
+    method           VARCHAR           ENCODE ZSTD,
+    page             VARCHAR           ENCODE ZSTD,
+    registration     DOUBLE PRECISION  ENCODE ZSTD,
+    session_id       INTEGER           ENCODE ZSTD,
+    song             VARCHAR           ENCODE ZSTD,
+    status           VARCHAR           ENCODE ZSTD,
+    ts               BIGINT            ENCODE ZSTD,
+    user_agent       VARCHAR           ENCODE ZSTD,
+    user_id          INTEGER           ENCODE ZSTD
+);
 """
 
 staging_songs_table_create = """
@@ -48,7 +48,7 @@ CREATE TABLE songs_stage (
     song_id           VARCHAR           ENCODE ZSTD,
     title             VARCHAR           ENCODE ZSTD,
     year              INTEGER           ENCODE ZSTD
-)
+);
 """
 
 songplay_table_create = """
@@ -57,8 +57,8 @@ CREATE TABLE songplays(
   start_time   TIMESTAMP               ENCODE DELTA32K,
   user_id      INTEGER                 ENCODE ZSTD,
   level        VARCHAR                 ENCODE ZSTD,
-  song_id      INTEGER                 ENCODE ZSTD,
-  artist_id    INTEGER                 ENCODE ZSTD,
+  song_id      VARCHAR                 ENCODE ZSTD,
+  artist_id    VARCHAR                 ENCODE ZSTD,
   session_id   INTEGER                 ENCODE ZSTD,
   location     VARCHAR                 ENCODE ZSTD,
   user_agent   VARCHAR                 ENCODE ZSTD
@@ -77,9 +77,9 @@ CREATE TABLE users (
 
 song_table_create = """
 CREATE TABLE songs(
-  song_id    INTEGER           ENCODE ZSTD,
+  song_id    VARCHAR           ENCODE ZSTD,
   title      VARCHAR           ENCODE ZSTD,
-  artist_id  INTEGER           ENCODE ZSTD,
+  artist_id  VARCHAR           ENCODE ZSTD,
   year       INTEGER           ENCODE ZSTD,
   duration   DOUBLE PRECISION  ENCODE ZSTD
 );
@@ -87,40 +87,40 @@ CREATE TABLE songs(
 
 artist_table_create = """
 CREATE TABLE artists (
-    song_id     INTEGER NOT NULL  ENCODE ZSTD, 
-    title       VARCHAR           ENCODE ZSTD,
-    artist_id   INTEGER           ENCODE ZSTD,
-    year        INTEGER           ENCODE ZSTD,
-    duration    DOUBLE PRECISION  ENCODE ZSTD
+    artist_id  VARCHAR           ENCODE ZSTD,
+    name       VARCHAR           ENCODE ZSTD,
+    location   VARCHAR           ENCODE ZSTD,
+    latitude   DOUBLE PRECISION  ENCODE ZSTD,
+    longitude  DOUBLE PRECISION  ENCODE ZSTD
 );
 """
 
 time_table_create = """
-CREATE TABLE TIME(
-    start_time  INTEGER  ENCODE ZSTD,
-    hour        INTEGER  ENCODE ZSTD,
-    day         INTEGER  ENCODE ZSTD,
-    week        INTEGER  ENCODE ZSTD,
-    month       INTEGER  ENCODE ZSTD,
-    year        INTEGER  ENCODE ZSTD,
-    weekday     INTEGER  ENCODE ZSTD
+CREATE TABLE time(
+    start_time  TIMESTAMP  ENCODE DELTA32K,
+    hour        INTEGER    ENCODE ZSTD,
+    day         INTEGER    ENCODE ZSTD,
+    week        INTEGER    ENCODE ZSTD,
+    month       INTEGER    ENCODE ZSTD,
+    year        INTEGER    ENCODE ZSTD,
+    weekday     INTEGER    ENCODE ZSTD
 );
 """
 
 
 # STAGING TABLES
-def build_copy_sql(table, filepath):
+def build_copy_sql(table, filepath, json_format):
     return f"""
-    COPY {table} 
+    COPY {table}
     FROM {filepath}
     IAM_ROLE {IAM_ROLE}
-    FORMAT AS JSON 'auto';
+    FORMAT AS JSON {json_format}
     """
 
 
-staging_events_copy = build_copy_sql('events_stage', LOG_DATA)
+staging_events_copy = build_copy_sql('events_stage', LOG_DATA, json_format=LOG_JSONPATH)
 
-staging_songs_copy = build_copy_sql('songs_stage', SONG_DATA)
+staging_songs_copy = build_copy_sql('songs_stage', SONG_DATA, json_format="'auto'")
 
 # FINAL TABLES
 
@@ -137,8 +137,88 @@ artist_table_insert = ("""
 """)
 
 time_table_insert = ("""
-""")
+# """)
 
+# Load song data from staging table into songs
+songs_load = """
+    INSERT INTO songs
+    SELECT DISTINCT
+        song_id,
+        title,
+        artist_id,
+        year,
+        duration
+    FROM songs_stage;
+"""
+
+# Load artist data from staging table into artists
+artists_load = """
+    INSERT INTO artists
+    SELECT DISTINCT
+        artist_id,
+        artist_name,
+        artist_location,
+        artist_latitude,
+        artist_longitude
+    FROM songs_stage;
+"""
+
+# Load unique users from staging table into users
+# For users with multiple levels, selects the latest level
+users_load = """
+    INSERT INTO users
+    SELECT DISTINCT
+        user_id,
+        first_name,
+        last_name,
+        gender,
+        level
+    FROM (
+        SELECT
+            ts,
+            user_id,
+            first_name,
+            last_name,
+            gender,
+            level
+        FROM events_stage
+        ORDER BY ts DESC
+    ) users_temp
+"""
+
+# Extract, convert, and split log timestamps to load into time table
+time_load = """
+    INSERT into TIME
+     SELECT DISTINCT
+       ts,
+       extract(hour FROM ts) as hour,
+       extract(day FROM ts) as day,
+       extract(week FROM ts) as week,
+       extract(month FROM ts) as month,
+       extract(year FROM ts) as year,
+       extract(dow FROM ts) as weekday
+    FROM (
+      SELECT timestamp 'epoch' + ts / 1000 * interval '1 second' AS ts
+      FROM events_stage
+    ) next_song_ts;
+"""
+
+# Load songplays data from staging table, joining on songs and artists
+songplays_load = """
+    INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    SELECT DISTINCT
+        timestamp 'epoch' + e.ts / 1000 * interval '1 second' AS ts,
+        e.user_id,
+        e.level,
+        s.song_id,
+        a.artist_id,
+        e.session_id,
+        e.location,
+        e.user_agent
+    FROM events_stage e
+    LEFT JOIN songs s ON s.title = e.song
+    LEFT JOIN artists a ON a.name = e.artist
+"""
 # QUERY LISTS
 
 create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create,
